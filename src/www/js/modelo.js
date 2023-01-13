2eab78988cfe238fda4d839deac791faa62cbef8
Jorge Ortega <jorge77.ortega@gmail.com>
2023-01-13 05:48:18 +0100

    MOckups y mas

--- src/www/js/modelo.js
+++ src/www/js/modelo.js
@@ -5,7 +5,13 @@
  * 
  */ 
 export class Modelo{
-    constructor(nombre){
+    constructor(nombre, talla, diaComprado, descripcion, enUso, tipo, estacion){
         this.nombre = nombre
+        this.talla = talla
+        this.diaComprado = diaComprado
+        this.descripcion = descripcion
+        this.enUso = enUso//al dar de alta se daria en false 
+        this.tipo= tipo //calzado parte de arriba... el select
+        this.estacion = estacion//invierno verano otoño primavera el checkbox
 	}
 }
